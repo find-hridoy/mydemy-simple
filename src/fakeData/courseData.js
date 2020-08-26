7f08d0b5ca7c857db9a2f8ba46986f715cf51d60
hridoy546 <mdhridoymia546@gmail.com>
2020-08-27 01:41:34 +0600

    first commit

---
+++ src/fakeData/courseData.js
@@ -0,0 +1,115 @@
+const courseData = [
+    {
+        id: 1,
+        title: "Google Analytics Training Course for Beginners",
+        name: "Samara Franzoni",
+        price: 86.6,
+        img: "https://i.ibb.co/7VWgMb9/1.jpg",
+        stockTime: 2
+    },
+    {
+        id: 2,
+        title: "IELTS Band 7+ Complete Prep Course",
+        name: "Tori Wetherby",
+        price: 138.4,
+        img: "https://i.ibb.co/Cv4L0cB/2.jpg",
+        stockTime: 6
+    },
+    {
+        id: 3,
+        title: "Desktop apps with Angular, Firestore and Electron",
+        name: "Elwood Hornyak",
+        price: 149.5,
+        img: "https://i.ibb.co/nm8fSwt/3.jpg",
+        stockTime: 6
+    },
+    {
+        id: 4,
+        title: "The Complete Android N Developer Course",
+        name: "Rene Hotard",
+        price: 132,
+        img: "https://i.ibb.co/7KMm5Cr/4.jpg",
+        stockTime: 4
+    },
+    {
+        id: 5,
+        title: "Web development and design – from Level 0",
+        name: "Charlie Primmer",
+        price: 186,
+        img: "https://i.ibb.co/PC41sZw/5.jpg",
+        stockTime: 9
+    },
+    {
+        id: 6,
+        title: "Autodesk Revit – Beginner to an intermediate level",
+        name: "Tobias Dubray",
+        price: 173.6,
+        img: "https://i.ibb.co/VMFmqLq/6.jpg",
+        stockTime: 4
+    },
+    {
+        id: 7,
+        title: "The Complete Introduction to Data Analytics with Tableau",
+        name: "Tableau",
+        price: 77.6,
+        img: "https://i.ibb.co/dDBD7Vn/7.jpg",
+        stockTime: 6
+    },
+    {
+        id: 8,
+        title: "Life Mastery – Happiness, Health & Success",
+        name: "Shery Ertel",
+        price: 38.5,
+        img: "https://i.ibb.co/C7Q7Ljk/8.jpg",
+        stockTime: 8
+    },
+    {
+        id: 9,
+        title: "The Complete Android & Java Developer Course – Build 21 Apps",
+        name: "Dominic Catania",
+        price: 56.6,
+        img: "https://i.ibb.co/bWm6fYF/9.jpg",
+        stockTime: 5
+    },
+    {
+        id: 10,
+        title: "JavaScript Algorithms and Data Structures Masterclass",
+        name: "Vince Sandrock",
+        price: 76.8,
+        img: "https://i.ibb.co/87Njkc9/10.jpg",
+        stockTime: 2
+    },
+    {
+        id: 11,
+        title: "Adobe Premiere Pro – The Complete Video Editing Master Class",
+        name: "Jonathon Blauvelt",
+        price: 169,
+        img: "https://i.ibb.co/f0L3nhL/11.jpg",
+        stockTime: 5
+    },
+    {
+        id: 12,
+        title: "Relationship SECRETS for Women in their 20s – Find LOVE FAST",
+        name: "Elidia Nose",
+        price: 58.6,
+        img: "https://i.ibb.co/n6tBP5J/12.jpg",
+        stockTime: 6
+    },
+    {
+        id: 13,
+        title: "Prototyping a single page website in photoshop from scratch",
+        name: "Ozie Elliott",
+        price: 68.8,
+        img: "https://i.ibb.co/hXRWNm6/13.jpg",
+        stockTime: 3
+    },
+    {
+        id: 14,
+        title: "Startup Business – How To Raise Seed Capital",
+        name: "Clair Crosswhite",
+        price: 99.5,
+        img: "https://i.ibb.co/7YQZK6z/14.jpg",
+        stockTime: 4
+    }
+]
+export default courseData;
